655b64fcfe82269476bf6ab45e31bd95e47fc07d
Rakibul Islam <rakibul241221@gmail.com>
2022-10-26 12:16:42 +0600

    create courses data set

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Skill Share Is Running !!')
 })
@@ -10,6 +13,7 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
+
 
 app.listen(port, () => {
     console.log('Skill Share Server Is Running on port ', port)
